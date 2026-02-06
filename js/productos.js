5f73dc685fc1fb82942c28bfa0dd39c6fe7928e5
Martin Leidreiter <marcus.contenido@gmail.com>
2026-02-06 13:32:54 -0300

    Actualización automática de productos - 6/2/2026, 01:32:53

--- js/productos.js
+++ js/productos.js
@@ -1,6 +1,6 @@
 // Base de datos de productos con galería de imágenes
 // Generado automáticamente desde Google Sheets
-// Última actualización: 29/1/2026, 02:32:05
+// Última actualización: 6/2/2026, 01:32:53
 
 const productos = [
     {
